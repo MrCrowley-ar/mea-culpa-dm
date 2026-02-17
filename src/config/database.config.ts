c8d23cda8ee0dc1865c2fa4098032f4df8a45890
MrCrowley <javier.beltrame.dev@gmail.com>
2026-02-17 11:41:26 -0300

    modify ddbb parameters

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('database', () => ({
   host: process.env.DB_HOST || 'localhost',
   port: parseInt(process.env.DB_PORT ?? '5432', 10),
-  username: process.env.DB_USERNAME || 'expediciones',
-  password: process.env.DB_PASSWORD || 'expediciones123',
-  database: process.env.DB_DATABASE || 'expediciones_db',
+  username: process.env.DB_USERNAME || 'user',
+  password: process.env.DB_PASSWORD || 'pass',
+  database: process.env.DB_DATABASE || 'mea-culpa-db',
 }));
